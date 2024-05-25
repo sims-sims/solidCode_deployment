864f3b26603269088ec1ca42414529f4d7898c71
Renai79 <140294481+Renai79@users.noreply.github.com>
2024-05-25 07:32:58 +0530

    Update mcatcommunity.sol

--- mcatcommunity.sol
+++ mcatcommunity.sol
@@ -2,7 +2,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract MztacatCommunity {
+contract MayaCommunity {
     string public name = "C8C2Community";
     string public symbol = "MAY";
     uint8 public decimals = 18;
